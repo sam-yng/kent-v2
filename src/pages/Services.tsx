c2367d94545a26eeb9db54864814729f188d06a4
sam-yng <young142001@hotmail.com>
2023-05-19 16:41:30 +1000

    services

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -7,6 +7,7 @@ import handshake from "../assets/icons/handshake.png";
 import tax from "../assets/icons/tax.png";
 import stamp from "../assets/icons/evaluate.png";
 import copy from "../utils/copy.json";
+import { Button } from "../components/Button";
 
 export const Services: React.FC = () => {
   return (
@@ -84,6 +85,76 @@ export const Services: React.FC = () => {
           20+ Years of Valuation Experience <br></br>within the Sydney Real{" "}
           <br></br>Estate Market
         </h1>
+      </section>
+      <section
+        className={classNames(
+          "font-roboto",
+          "text-xl",
+          "text-slate-600",
+          "tracking-wider",
+          "md:w-[40vw]",
+          "md:mx-auto",
+          "mt-16",
+          "mb-16",
+          "space-y-8"
+        )}
+      >
+        <h1 className="font-playfairregular text-4xl text-slate-700 text-center">
+          Commercial Valuations
+        </h1>
+        <p>{JSON.parse(JSON.stringify(copy.services[0].para13))}</p>
+        <p>{JSON.parse(JSON.stringify(copy.services[0].para14))}</p>
+        <p>{JSON.parse(JSON.stringify(copy.services[0].para15))}</p>
+        <p>{JSON.parse(JSON.stringify(copy.services[0].para16))}</p>
+      </section>
+      <section
+        className={classNames(
+          "bg-gray-50",
+          "font-roboto",
+          "text-xl",
+          "text-center",
+          "pt-12",
+          "pb-12"
+        )}
+      >
+        <div className="mx-auto md:w-[25vw] space-y-6">
+          <h1
+            className={classNames(
+              "text-center",
+              "font-playfairregular",
+              "tracking-widest",
+              "text-4xl"
+            )}
+          >
+            Other <br></br>Services.
+          </h1>
+          <p>{JSON.parse(JSON.stringify(copy.services[0].para17))}</p>
+          <p>{JSON.parse(JSON.stringify(copy.services[0].para18))}</p>
+        </div>
+        <div className="border-sky-800 border md:w-[25vw] mx-6 md:mx-auto mt-6 mb-6" />
+        <div
+          className={classNames(
+            "grid",
+            "grid-cols-2",
+            "grid-rows-2",
+            "mx-auto",
+            "md:w-[25vw]"
+          )}
+        >
+          <p className="my-6">
+            Self Mangaged <br></br>Superannuation <br></br>Funds
+          </p>
+          <p className="my-6">
+            Rental <br></br>Inspections <br></br>Conditional <br></br>Reports
+          </p>
+          <p className="my-6">
+            General Building <br></br>Insurance
+          </p>
+          <p className="my-6">
+            Land Tax <br></br>Valuations
+          </p>
+        </div>
+        <Button text="Get a Quote" />
       </section>
     </>
   );
